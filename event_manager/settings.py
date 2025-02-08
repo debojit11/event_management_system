1981d020e75591f4e31fdfa001390c4858cb4ea6
Debojit <debojitchoudhury117@gmail.com>
2025-02-08 15:13:39 +0000

    prod testing with custom domain

--- event_manager/settings.py
+++ event_manager/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = ["eventmanagement.site", "www.eventmanagement.site"]
 
 
 # Application definition
